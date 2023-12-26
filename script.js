7e34e5ff73bae5767af8dfd699caf760838e961b
Ganesh2048 <129187187+Ganesh2048@users.noreply.github.com>
2023-12-26 21:40:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function fibonacci(num) {
-	if(num=1)
+	if(num===1)
 		return 0;
-	else if(num==2)
+	else if(num===2)
 		return 1;
 	else
 	{
 	let prev1=0;
 	let prev2=1;
 	let sum=0;
-	for (let i = 3; i <= num; i++)
+	for (let i = 2; i < num; i++)
 		{
 			sum=prev1+prev2;
 			prev1=prev2;
 			prev2=sum;
 		}
-		return sum;
-		
+		return sum; 
+		 
 	}
 }
 // your code here
 
-var num=prompt(Enter a num:);
+let num=prompt()
 //module.exports = fibonacci;
 alert(fibonacci(num));
